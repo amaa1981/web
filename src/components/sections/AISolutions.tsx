e822cde69b7ab9bc52725436f2a5f263473a7f79
amaa1981 <amaa_1981@hotmail.com>
2026-01-21 14:44:48 +0300

    Changed the Safety

--- src/components/sections/AISolutions.tsx
+++ src/components/sections/AISolutions.tsx
@@ -29,8 +29,8 @@ const features: Feature[] = [
     ),
   },
   {
-    name: "Industrial Safety & Security",
-    description: "AI-powered surveillance to detect unauthorized access and safety hazards in industrial environments.",
+    name: "Industrial Safety & Compliance",
+    description: "Protect your workforce with real-time AI monitoring that ensures adherence to safety protocols by automatically detection.",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" className="size-6">
         <path style={{ stroke: '#13787F' }} strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.286Zm0 0v1.5c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-1.5" />
